160e6b9fb650fa6abd75dec5243afb95def1d68e
Alex Kovshovik <alex@shovik.com>
2016-05-04 19:14:24 -0400

    Add babel ES2015 compilation for dev mode to support Safari

--- webpack.config.js
+++ webpack.config.js
@@ -71,7 +71,15 @@ if ( TARGET_ENV === 'development' ) {
           ]
         },
         { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
-        { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+        { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+        {
+          test: /\.js?$/,
+          exclude: /(node_modules)/,
+          loader: 'babel',
+          query: {
+            presets: ['es2015']
+          }
+        }
       ]
     }
 
