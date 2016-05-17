82366abd263704be0bb484e30f56574cc88a903e
Alex Kovshovik <alex@shovik.com>
2016-05-17 12:23:48 -0400

    Fix deployment for both production and staging

--- webpack.config.js
+++ webpack.config.js
@@ -110,7 +110,7 @@ if ( TARGET_ENV === 'production' ) {
           ])
         },
         { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
-        { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+        { test: /\.(ttf|eot|svg|jpg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
         {
           test: /\.js?$/,
           exclude: /(node_modules)/,
